8f0b2ff1182a0dfc8e36c1f3cf1c60d3e8b634df
Ivan Tymofiiev <ivanco77@gmail.com>
2026-02-24 18:21:39 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,35 @@
+/*
+========================================================================
+Create Database and Schemas
+========================================================================
+WARNING:
+  DataWarehouse database will drop the entire DB.
+*/
 
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'Datawarehouse')
+BEGIN
+   ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+   DROP DATABASE DataWarehouse;
+END;
+GO
+  
+-- Create Database 'DataWarehouse'
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
